6765e7f986ddd777b64010d1fc47d4d9d52c90c9
eugenos-programos <elena.samchuk.73@mail.ru>
2023-12-27 14:33:06 +0300

    refactor: rename tp,fp,tn.fn variables + add docstrings

--- BooleanSearch/MetricCalculater.py
+++ BooleanSearch/MetricCalculater.py
@@ -7,34 +7,27 @@ import matplotlib.pyplot as plt
 
 class Metrics:
     def __init__(self, foundRelevant, foundNotRelevant, notFoundRelevant, notFoundNotRelevant):
-        self.a = foundRelevant
-        self.b = notFoundRelevant
-        self.c = foundNotRelevant
-        self.d = notFoundNotRelevant
+        self.tp = foundRelevant
+        self.fp = notFoundRelevant
+        self.tn = foundNotRelevant
+        self.fn = notFoundNotRelevant
 
     def getRecall(self):
-        return self.a / (self.a + self.c)
+        return self.tp / (self.tp + self.tn)
 
     def getPrecision(self):
-        return self.a / (self.a + self.b)
+        return self.tp / (self.tp + self.fp)
 
     def getAccuracy(self):
-        return (self.a + self.d) / (self.a + self.b + self.c + self.d)
+        return (self.tp + self.fn) / (self.tp + self.fp + self.tn + self.fn)
 
     def getError(self):
-        return (self.b + self.c) / (self.a + self.b + self.c + self.d)
+        return (self.fp + self.tn) / (self.tp + self.fp + self.tn + self.fn)
 
     def getF_measure(self):
         return 2 / (1 / self.getPrecision() + 1 / self.getRecall())
 
     def getAveragePrecision(self, k, positions):
-        """
-        Вычисляет среднюю точность (average precision).
-
-        :param k: Количество релевантных документов
-        :param positions: Список позиций, на которых релевантные документы были найдены
-        :return: Значение средней точности
-        """
         total_precision = 0.0
         num_relevant_docs_found = 0
 
@@ -49,21 +42,14 @@ class Metrics:
         return total_precision / num_relevant_docs_found
 
     def calculateInterpolatedPrecision(self, num_relevant_docs, ranked_relevance):
-        """
-        Рассчитывает интерполированные значения точности для заданных уровней полноты.
-
-        :param num_relevant_docs: Количество релевантных документов в коллекции
-        :param ranked_relevance: Список булевых значений, где True означает, что документ релевантен
-        :return: Список интерполированных значений точности
-        """
-        recall_levels = np.arange(0, 1.1, 0.1)  # Уровни полноты от 0 до 1 с шагом 0.1
+        recall_levels = np.arange(0, 1.1, 0.1)
         interpolated_precisions = []
 
         for recall_level in recall_levels:
             recall_threshold = int(recall_level * num_relevant_docs)
             relevant_results = ranked_relevance[:recall_threshold]
             precision = sum(relevant_results) / (
-                        recall_threshold + 1e-8)  # Добавляем 1e-8 для избежания деления на ноль
+                        recall_threshold + 1e-8) 
             interpolated_precisions.append(precision)
 
         return interpolated_precisions
